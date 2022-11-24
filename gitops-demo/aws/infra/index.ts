6952fd83fdefd0561fabfddf07e11706ddbca5bd
andtell <andreas.tell@callistaenterprise.se>
2022-11-24 17:47:49 +0100

    added some stuff

--- gitops-demo/aws/infra/index.ts
+++ gitops-demo/aws/infra/index.ts
@@ -1,9 +1,16 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws-native";
+// import * as aws from "@pulumi/aws-native";
+import * as aws from "@pulumi/aws";
+import { EventRule } from "@pulumi/aws/cloudwatch";
 
 // Create an AWS resource (S3 Bucket)
-const bucket1 = new aws.s3.Bucket("my-gitops-bucket-test1");
+const bucket1 = new aws.s3.Bucket("my-gitops-bucket-test1", {
+    tags: {
+        "what": "ever"
+    }
+});
 const bucket2 = new aws.s3.Bucket("my-gitops-bucket-test2");
+
 
 // Export the name of the bucket
 export const bucketName1 = bucket1.id;
