6b7ee75f61ea2f41ea7a91f580fdc680f799bc9c
andtell <andreas.tell@callistaenterprise.se>
2022-11-24 13:30:46 +0100

    renamed bucket again

--- gitops-demo/aws/infra/index.ts
+++ gitops-demo/aws/infra/index.ts
@@ -2,7 +2,7 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws-native";
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-renamed2-gitops-bucket");
+const bucket = new aws.s3.Bucket("my-renamed-gitops-bucket");
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
