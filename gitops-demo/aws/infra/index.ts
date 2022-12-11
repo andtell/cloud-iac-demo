fd0900bade2dab11aec4a88db00b9e9c15079a68
andtell <andreas.tell@callistaenterprise.se>
2022-12-11 21:56:26 +0100

    creating buckets from array

--- gitops-demo/aws/infra/index.ts
+++ gitops-demo/aws/infra/index.ts
@@ -1,22 +1,20 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
-// Create an AWS resource (S3 Bucket)
-export const bucket1 = new aws.s3.Bucket("my-gitops-bucket-test1",{
-    tags : {
-        Cadec: '2023',
-        Environment: pulumi.StackReference.name,
-        Name: 'wat',
-    },
-});
-export const bucket2 = new aws.s3.Bucket("my-gitops-bucket-test2",{
-    tags : {
-        Cadec: '2023',
-        Environment: pulumi.StackReference.name,
-        Name: 'wat',
-    },
-});
 
-// Export the name of the bucket
-export const bucketName1 = bucket1.id;
-export const bucketName2 = bucket2.id;
+export const bucketMap = new Map<string, aws.s3.Bucket>();
+
+["imperative", "infrastructure", "declaration"].forEach( s => {
+    bucketMap.set(s, createBucket(s));
+})
+
+// Create an AWS resource (S3 Bucket)
+function createBucket(name: string) : aws.s3.Bucket {
+    return new aws.s3.Bucket(name, {
+        tags : {
+            Cadec: '2023',
+            Environment: pulumi.StackReference.name,
+            Name: name,
+        },
+    });
+}
