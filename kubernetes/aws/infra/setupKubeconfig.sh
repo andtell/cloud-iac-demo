aa6d3e76beceb9aa053b5f30e59563faf454d527
andtell <andreas.tell@callistaenterprise.se>
2023-01-02 17:07:44 +0100

    argocd deplot on EKS

---
+++ kubernetes/aws/infra/setupKubeconfig.sh
@@ -0,0 +1,17 @@
+pulumi stack output kubeconfig --show-secrets > kubeconfig.yml
+
+echo "Rember to 'source' this file in order to make env vars stick after exit."
+
+export KUBECONFIG=$PWD/kubeconfig.yml
+
+echo $KUBECONFIG
+
+kubectl get svc
+
+
+# https://eu-north-1.console.aws.amazon.com/vpc/home?region=eu-north-1#subnets:search=eks;sort=SubnetId
+
+# aws eks describe-cluster --name eks-cluster-eksCluster-0d14d05 
+
+
+# Maybe? https://stackoverflow.com/questions/72176710/eks-update-config-with-awscli-command-aws-eks-update-kubeconfig-fails-with-err
